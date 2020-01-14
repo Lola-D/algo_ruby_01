22d9db00952004aed9de4d96061f8fe888b508c1
Lola-D <lola.donval@gmail.com>
2020-01-14 16:48:28 +0100

    second commit

--- exo_21.rb
+++ exo_21.rb
@@ -1,14 +1,15 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 
-num = Integer(gets.chomp)
-i = 1
-symbol = [1] * num
-
-symbol.each do |number|
-  (num - 1).times {print " "}
-  i.times {print "#"}
-  puts "\n"
-  num -= 1
-  i += 1
-end 
+def half_pyramid
+  num = Integer(gets.chomp)
+  i = 1
+  symbol = [1] * num
+  symbol.each do |number|
+    (num - 1).times {print " "}
+    i.times {print "#"}
+    puts "\n"
+    num -= 1
+    i += 1
+  end 
+end
